85423ad5e388d732750cf56a4450906cd75e290e
Christian Hernandez <cnhz95@gmail.com>
2026-05-28 21:56:57 +0200

    First commit

---
+++ src/main/java/com/example/visitedcountries/VisitedCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.visitedcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitedCountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitedCountriesApplication.class, args);
+    }
+
+}
