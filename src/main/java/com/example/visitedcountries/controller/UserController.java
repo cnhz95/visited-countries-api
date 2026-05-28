7b9143e881a1809ea08f7fe1cc80b525a8c72202
Christian Hernandez <cnhz95@gmail.com>
2026-05-28 23:06:25 +0200

    Add User controller and service

---
+++ src/main/java/com/example/visitedcountries/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.visitedcountries.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {}
