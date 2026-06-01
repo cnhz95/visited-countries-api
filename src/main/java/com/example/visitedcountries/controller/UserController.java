5fed8a0f0d35bebc733a89aea495ac39a305b57d
Christian Hernandez <cnhz95@gmail.com>
2026-06-01 22:14:02 +0200

    Add endpoint to list all users

--- src/main/java/com/example/visitedcountries/controller/UserController.java
+++ src/main/java/com/example/visitedcountries/controller/UserController.java
@@ -1,10 +1,23 @@
 package com.example.visitedcountries.controller;
 
+import com.example.visitedcountries.entity.User;
+import com.example.visitedcountries.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-public class UserController {}
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+}
--- src/main/java/com/example/visitedcountries/service/UserService.java
+++ src/main/java/com/example/visitedcountries/service/UserService.java
@@ -1,7 +1,18 @@
 package com.example.visitedcountries.service;
+
+import com.example.visitedcountries.entity.User;
+import com.example.visitedcountries.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class UserService {}
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
