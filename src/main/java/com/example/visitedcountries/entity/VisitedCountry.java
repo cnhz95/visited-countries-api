6002fd0c4a9b5d19e10462ebcfa70d63b01fcbcc
Christian Hernandez <cnhz95@gmail.com>
2026-06-01 21:54:06 +0200

    Remove commented-out code

--- src/main/java/com/example/visitedcountries/entity/VisitedCountry.java
+++ src/main/java/com/example/visitedcountries/entity/VisitedCountry.java
@@ -5,14 +5,11 @@ import lombok.*;
 
 @Entity
 @Table(name = "visited_countries")
-//@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-//@ToString(exclude = {"user", "country"})
-//@EqualsAndHashCode(exclude = {"user", "country"})
 public class VisitedCountry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,12 +17,10 @@ public class VisitedCountry {
 
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
-    //@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private User user;
 
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "country_id", nullable = false)
-    //@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Country country;
 
     @Column
